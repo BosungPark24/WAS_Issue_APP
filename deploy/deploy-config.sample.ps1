977995f6a830c13263a6c7baa38d3d2d2b467906
bosungpark <bspark631@gmail.com>
2026-03-04 13:12:29 +0900

    FIX SSH key parsing in GitHub Action

--- deploy/deploy-config.sample.ps1
+++ deploy/deploy-config.sample.ps1
@@ -1,19 +1,18 @@
 $MvnCmd = "mvn"
 
 $RemoteHost = "10.20.210.239"
-$RemoteUser = "weblogic"
+$RemoteUser = "bs.park2"
 $SshKeyPath = "$env:USERPROFILE\.ssh\id_rsa"
 
-$RemoteDeployDir = "/home/weblogic/deployments/bosung-app"
+$RemoteDeployDir = "/home/bs.park2/apps/deployments/bosung-app"
 $RemoteWarPath = "$RemoteDeployDir/bosung-app.war"
 $RemoteDeployScriptPath = "$RemoteDeployDir/deploy_remote.sh"
 
-$DomainHome = "/u01/oracle/user_projects/domains/base_domain"
-$AdminUrl = "t3://10.20.210.239:7001"
-$TargetName = "AdminServer"
+$DomainHome = "/home/bs.park2/weblogic1411/wls14110/domains/base_domain"
+$AdminUrl = "t3://10.20.210.239:20001"
+$TargetName = "Cluster-0"
 $AppName = "bosung-app"
 
-$WlsUserConfigFile = "/home/weblogic/.wls/userConfig.secure"
-$WlsUserKeyFile = "/home/weblogic/.wls/userKey.secure"
+$BootPropertiesPath = "/home/bs.park2/weblogic1411/wls14110/domains/base_domain/boot.properties"
 
 $LocalWarPath = "target/bosung-app.war"
